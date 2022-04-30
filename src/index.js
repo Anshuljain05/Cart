7cb534edb1809e10a7b62e50a33e8f39dac3e72b
Anshuljain05 <anshuljain05@outlook.com>
2022-04-30 21:48:42 +0530

    Add Firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApZogBexywra76X0EvNlR-qGl3QvC4gkw",
+  authDomain: "cart-b3760.firebaseapp.com",
+  projectId: "cart-b3760",
+  storageBucket: "cart-b3760.appspot.com",
+  messagingSenderId: "865767282010",
+  appId: "1:865767282010:web:b79f7cf92cf6e81d720f63"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
